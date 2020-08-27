b1a725d766dbe17642617c2ba5fbb2e3480fda1c
Lionell <lionell_loh@mymail.sutd.edu.sg>
2020-08-28 01:33:10 +0800

    invoke importlib.find_spec() to differentiate local and non-local imports

--- ast_example.py
+++ ast_example.py
@@ -1,26 +1,24 @@
 import ast
 import sys
 import re
-import glob
 import os
+import importlib
+
+from platform import python_version
 from pypi_simple import PyPISimple
 from piptools.scripts.compile import cli
-from platform import python_version
 from stdlib_list import stdlib_list
+from wcmatch import wcmatch
 
 def get_python_files(abs_path: str) -> list:
     """:arg
     path_arg: absolute directory of the code base
     return a list of paths that are organic to the code base
     """
-    dep_paths_pattern = "/".join([abs_path, "**/[site-packages|node_modules]/**/*.py"])
-    all_paths_pattern = "/".join([abs_path, "**/*.py"])
-    dep_paths = glob.glob(dep_paths_pattern, recursive=True)
-    all_paths = glob.glob(all_paths_pattern, recursive=True)
+    # TODO: try not to hardcode this deny list 'site-packages | node_modules'
+    paths = wcmatch.WcMatch(abs_path, "*.py", "site-packages|node_modules", flags=wcmatch.RECURSIVE).match()
 
-    organic_paths = list(set(all_paths) - set(dep_paths))
-    print(organic_paths)
-    return organic_paths
+    return paths
 
 
 def get_builtin_libs(python_version: str) -> set:
@@ -32,18 +30,38 @@ def get_builtin_libs(python_version: str) -> set:
     return set(top_level_libs)
 
 
-def generate_requirements_file(import_list: list, dest_file):
+def generate_requirements_file(import_list: list, dest_file: str, code_dir_abs_path: str):
     if not os.path.exists(dest_file):
         with open(dest_file, 'w'):
             pass
+    # insert in the path so python will prioritise the search accordingly
+    sys.path.insert(0, code_dir_abs_path)
+    print(sys.path, end = "\n\n")
+    local_import_set = set()
+    faulty_imports = set()
+    # normalize api.utils -> api and then remove duplicates
+    import_set = set([imp.split(".")[0] for imp in import_list])
+    print(import_set, end = "\n\n")
+    for imp in import_set:
+        module_spec = importlib.util.find_spec(imp)
+        # What happens if u run this in a virtualenv that does not contain the package?
+        if module_spec is None or module_spec.origin is None:
+            faulty_imports.add(imp)
+            continue
+        print(module_spec)
+        if module_spec.origin[:len(code_dir_abs_path)] == code_dir_abs_path:
+            local_import_set.add(imp)
 
+    import_list = list(import_set - local_import_set - faulty_imports)
+    # print("IMPORT LIST", import_list)
+    # print("FAULTY IMPORTS", faulty_imports)
     vers = ".".join(python_version().split(".")[:-1])
     builtin_lst = get_builtin_libs(vers)
     non_builtin_lst = list(set(import_list).difference(builtin_lst))
 
     client = PyPISimple()
     pip_package_list = sorted([pkg for pkg in non_builtin_lst if client.get_project_files(pkg) != []])
-
+    pip_package_list = [pkg.replace("_", "-") for pkg in pip_package_list]
     with open(dest_file, 'w') as f:
         for pkg in pip_package_list:
             f.write(f"{pkg}\n")
@@ -52,6 +70,8 @@ def generate_requirements_file(import_list: list, dest_file):
 
     # Need to change the args before running pip-compile
     sys.argv = ["", dest_file]
+    print(pip_package_list)
+    print("Generating dependencies from top-level imports...")
     cli()
 
 
@@ -76,7 +96,7 @@ def main():
 
     import_list = analyzer.report()
 
-    generate_requirements_file(import_list, requirements_file)
+    generate_requirements_file(import_list, requirements_file, abs_path)
 
 
 """
