82f5af3931c8fa1a4aae075687fed7077c95163b
Lionell <lionell_loh@mymail.sutd.edu.sg>
2020-08-28 16:46:59 +0800

    Can differentiate local from not local correctly now

--- ast_example.py
+++ ast_example.py
@@ -3,6 +3,7 @@ import sys
 import re
 import os
 import importlib
+from collections import defaultdict
 
 from platform import python_version
 from pypi_simple import PyPISimple
@@ -10,7 +11,8 @@ from piptools.scripts.compile import cli
 from stdlib_list import stdlib_list
 from wcmatch import wcmatch
 
-def get_python_files(abs_path: str) -> list:
+
+def get_python_filenames(abs_path: str) -> list:
     """:arg
     path_arg: absolute directory of the code base
     return a list of paths that are organic to the code base
@@ -30,38 +32,65 @@ def get_builtin_libs(python_version: str) -> set:
     return set(top_level_libs)
 
 
-def generate_requirements_file(import_list: list, dest_file: str, code_dir_abs_path: str):
+def flatten_nested_lst(nested_lst: list) -> list:
+    ret = []
+    for lst in nested_lst:
+        ret.extend(lst)
+
+    return ret
+
+
+def generate_requirements_file(import_mapping: dict, dest_file: str, code_dir_abs_path: str):
     if not os.path.exists(dest_file):
         with open(dest_file, 'w'):
             pass
-    # insert in the path so python will prioritise the search accordingly
     sys.path.insert(0, code_dir_abs_path)
-    print(sys.path, end = "\n\n")
+    print(import_mapping)
+    print(sys.path, end="\n\n")
     local_import_set = set()
     faulty_imports = set()
+    import_list = flatten_nested_lst(list(import_mapping.values()))
     # normalize api.utils -> api and then remove duplicates
     import_set = set([imp.split(".")[0] for imp in import_list])
-    print(import_set, end = "\n\n")
-    for imp in import_set:
-        module_spec = importlib.util.find_spec(imp)
-        # What happens if u run this in a virtualenv that does not contain the package?
-        if module_spec is None or module_spec.origin is None:
-            faulty_imports.add(imp)
-            continue
-        print(module_spec)
-        if module_spec.origin[:len(code_dir_abs_path)] == code_dir_abs_path:
-            local_import_set.add(imp)
+    print(import_set, end="\n\n")
 
+    for dir, imps in import_mapping.items():
+        print(dir)
+        # insert in the path so python will prioritise the search accordingly
+        sys.path.insert(0, dir)
+        for imp in imps:
+            top_level_imp = imp.split(".")[0]
+            module_spec = importlib.util.find_spec(top_level_imp)
+            if not module_spec:
+                print("Faulty", top_level_imp, module_spec)
+                faulty_imports.add(top_level_imp)
+                continue
+
+            print(module_spec)
+            # Local import: found in origin
+            if module_spec.origin and module_spec.origin[:len(code_dir_abs_path)] == code_dir_abs_path:
+            # if module_spec.origin:
+                print("origin", module_spec.origin, "|", code_dir_abs_path)
+                local_import_set.add(imp)
+
+            # Local import: found in submodule_search_locations
+            elif module_spec.submodule_search_locations:
+                for search_loc in module_spec.submodule_search_locations:
+                    print(search_loc)
+                    if search_loc[:len(code_dir_abs_path)] == code_dir_abs_path:
+                        local_import_set.add(imp)
+
+    print(sys.path, end ="\n\n")
+    print("Faulty imports", faulty_imports)
+    print("Local imports", local_import_set)
     import_list = list(import_set - local_import_set - faulty_imports)
-    # print("IMPORT LIST", import_list)
-    # print("FAULTY IMPORTS", faulty_imports)
     vers = ".".join(python_version().split(".")[:-1])
     builtin_lst = get_builtin_libs(vers)
     non_builtin_lst = list(set(import_list).difference(builtin_lst))
 
     client = PyPISimple()
     pip_package_list = sorted([pkg for pkg in non_builtin_lst if client.get_project_files(pkg) != []])
-    pip_package_list = [pkg.replace("_", "-") for pkg in pip_package_list]
+
     with open(dest_file, 'w') as f:
         for pkg in pip_package_list:
             f.write(f"{pkg}\n")
@@ -76,6 +105,13 @@ def generate_requirements_file(import_list: list, dest_file: str, code_dir_abs_p
 
 
 def main():
+    # Remove 1th element
+    if len(sys.path) >= 3:
+        sys.path = sys.path[2:]
+
+    else:
+        sys.path = []
+
     assert len(sys.argv) == 3, "[USAGE]: Ensure you provide 2 arguments " \
                                "<directory of codebase> <filepath for requirements>"
 
@@ -85,39 +121,51 @@ def main():
         abs_path = sys.argv[1]
 
     requirements_file = sys.argv[2]
-    files = get_python_files(abs_path)
+    filenames = get_python_filenames(abs_path)
     analyzer = Analyzer()
 
-    for file in files:
-        print(f"Analyzing File: {file}")
-        with open(file, "r") as source:
+    for filename in filenames:
+        print(f"Analyzing File: {filename}")
+        with open(filename, "r") as source:
             tree = ast.parse(source.read())
             analyzer.visit(tree)
+            dir = "/".join(filename.split("/")[:-1])
+            analyzer.store_and_empty(dir)
 
-    import_list = analyzer.report()
+    import_mapping = analyzer.report()
 
-    generate_requirements_file(import_list, requirements_file, abs_path)
+    generate_requirements_file(import_mapping, requirements_file, abs_path)
 
 
 """
 Check docs at https://greentreesnakes.readthedocs.io/en/latest/nodes.html
 to understand node object attributes. 
 """
+
+
 class Analyzer(ast.NodeVisitor):
     def __init__(self):
-        self.imports = []
+        self.import_mapping = defaultdict(list)
+        self.import_cache = []
 
     def visit_Import(self, node):
         for alias in node.names:
-            self.imports.append(alias.name)
+            self.import_cache.append(alias.name)
         self.generic_visit(node)
 
     def visit_ImportFrom(self, node):
-        self.imports.append(node.module)
+        self.import_cache.append(node.module)
         self.generic_visit(node)
 
+    def empty_import_cache(self):
+        self.import_cache = []
+
+    def store_and_empty(self, dir: str):
+        self.import_mapping[dir].extend(self.import_cache)
+        self.empty_import_cache()
+
     def report(self):
-        return self.imports
+        return self.import_mapping
 
 
 if __name__ == "__main__":
